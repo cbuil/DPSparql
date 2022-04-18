27f98c8f9510e2df905262596f1b0295a8a1d5e3
Carlos Buil Aranda <cbuil@Carloss-MacBook-Pro.local>
2022-04-18 10:28:46 -0400

    updated queries

--- resources/run_count.py
+++ resources/run_count.py
@@ -2,7 +2,7 @@ import json
 import sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-def run_count(json_query_file):
+def run_count(json_query_file, output_file):
     with open(json_query_file, 'r') as f:
         queries = json.load(f)
         results = {}
@@ -11,11 +11,15 @@ def run_count(json_query_file):
                 sparql = SPARQLWrapper(query["endpoint"])
                 sparql.setQuery(query["query"])
                 sparql.setReturnFormat(JSON)
+                print("query: {} variables {} endpoint: {}".format(query['query'], query['variable'], query["endpoint"]))
                 query_results = sparql.query().convert()
                 for query_result in query_results["results"]["bindings"]:
                     if not query['query'].replace(" ", "").lower() in results:
                         results[query["query"].replace(" ", "").lower()] = [query_result[query['variable']]['value'], query['comment'], query['variable'], query['endpoint'], query['query']]
-    with open('data.json', 'w') as f:
+                        results[query["query"].replace(" ", "").lower()].append(query['id'])
+                    else:
+                        results[query["query"].replace(" ", "").lower()].append(query['id'])
+    with open(output_file, 'w') as f:
         json.dump(results, f, indent=4)
 
 if __name__ == '__main__':
@@ -23,4 +27,4 @@ if __name__ == '__main__':
         print('Usage: python run_count.py json_query_file ')
     else:
         print(sys.argv[1])
-        run_count(sys.argv[1])
+        run_count(sys.argv[1], sys.argv[2])
